8a5fba8cc84a7195fadbc373dca202c9910613cc
mahrshSenanayake <mahesh.tharanga92@gmail.com>
2023-01-26 00:47:25 +0530

    Remove Login Signup Section

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -127,14 +127,14 @@ const Header = () => {
               Blog
             </LinkScroll>
           </ul>
-          <div className="col-start-10 col-end-12 font-medium flex justify-end items-center">
+          {/* <div className="col-start-10 col-end-12 font-medium flex justify-end items-center">
             <Link href="/">
               <a className="text-black-600 mx-2 sm:mx-4 capitalize tracking-wide hover:text-orange-500 transition-all">
                   Sign In
               </a>
             </Link>
             <ButtonOutline>Sign Up</ButtonOutline>
-          </div>
+          </div> */}
         </nav>
       </header>
       {/* Mobile Navigation */}

fdfae0522da438db7a705de59e47298e15f372b1
mahrshSenanayake <mahesh.tharanga92@gmail.com>
2023-02-03 19:17:36 +0530

    Update footer as customer requirment

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -28,7 +28,7 @@ const Header = () => {
             <Link href="/"><img alt="nextui logo" src="/assets/scholarnest-logo.jpg" /></Link>
             
           </div>
-          <ul className="hidden lg:flex col-start-4 col-end-8 text-black-500  items-center">
+          <ul className="hidden lg:flex col-start-4 col-end-8 text-black-100  items-center">
             <LinkScroll
               activeClass="active"
               to="/company"
@@ -43,7 +43,7 @@ const Header = () => {
                 "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
                 (activeLink === "about"
                   ? " text-orange-500 animation-active "
-                  : " text-black-500 hover:text-orange-500 a")
+                  : " text-black-100 hover:text-orange-500 a")
               }
             >
               Company
@@ -62,7 +62,7 @@ const Header = () => {
                 "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
                 (activeLink === "pricing"
                   ? " text-orange-500 animation-active "
-                  : " text-black-500 hover:text-orange-500 ")
+                  : " text-black-100 hover:text-orange-500 ")
               }
             >
               Solutions
@@ -82,7 +82,7 @@ const Header = () => {
                 "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
                 (activeLink === ""
                   ? " text-orange-500 animation-active "
-                  : " text-black-500 hover:text-orange-500 ")
+                  : " text-black-100 hover:text-orange-500 ")
               }
             >
               Contact Us
@@ -102,7 +102,7 @@ const Header = () => {
                 "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
                 (activeLink === "feature"
                   ? " text-orange-500 animation-active "
-                  : " text-black-500 hover:text-orange-500 ")
+                  : " text-black-100 hover:text-orange-500 ")
               }
             >
               Learn with Us
@@ -121,7 +121,7 @@ const Header = () => {
                 "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
                 (activeLink === "feature"
                   ? " text-orange-500 animation-active "
-                  : " text-black-500 hover:text-orange-500 ")
+                  : " text-black-100 hover:text-orange-500 ")
               }
             >
               Career
@@ -148,7 +148,7 @@ const Header = () => {
                 "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
                 (activeLink === ""
                   ? " text-orange-500 animation-active "
-                  : " text-black-500 hover:text-orange-500 ")
+                  : " text-black-100 hover:text-orange-500 ")
               }
             >
               Blog

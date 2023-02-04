29b499ee559ce1b15ac68218f2528be49ead6f8d
rangasl <ranga.dayawansha@gmail.com>
2023-02-04 13:26:24 +0530

    career page

--- pages/career.js
+++ pages/career.js
@@ -1,11 +1,14 @@
 import Header from "../components/Layout/Header";
 import Footer from "../components/Layout/Footer";
+import Careers from "../components/Careers";
 
 function Career() {
   return (
     <>
       <Header />
-      <div style={{ paddingTop: "5%", paddingBottom: "25%" }}>Career</div>
+      <div style={{paddingTop:'5%',paddingBottom:'25%'}}>
+      <Careers/>
+      </div>
       <Footer />
     </>
   );

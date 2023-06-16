08252d484cb4198f3cc943b1b7a07d9208c8b0dc
ScholarNest <86905722+ScholarNest@users.noreply.github.com>
2023-06-16 17:15:16 +0530

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -6,7 +6,6 @@ import getScrollAnimation from "../utils/getScrollAnimation";
 import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
 import Lottie from "lottie-react";
 import landingLottie from "./landing-animation.json";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
 const Hero = ({
